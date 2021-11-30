1b07fe3a6c475beca09faca8c06a2967486197f9
BJBRITT82 <46686019+BJBRITT82@users.noreply.github.com>
2021-11-29 19:17:30 -0600

    Finished Assessment

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,88 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Brandon Britt" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-
+let totalCookies=gb+cc+sugar
+//Buttons
+const gbUp = document.querySelector('#add-gb')
+const gbDown = document.querySelector('#minus-gb')
+const ccUp = document.querySelector('#add-cc')
+const ccDown = document.querySelector('#minus-cc')
+const sugarUp = document.querySelector('#add-sugar')
+const sugarDown = document.querySelector('#minus-sugar')
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+//update quantity functions
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+function gbUpdate(gbQuantity){
+    let qty_gb = document.querySelector('#qty-gb')
+    qty_gb.innerHTML=gbQuantity
+}
+function ccUpdate(ccQuantity){
+    let qty_cc = document.querySelector('#qty-cc')
+    qty_cc.innerHTML=ccQuantity
+}
+function sugarUpdate(sugarQuantity){
+    let qty_sugar = document.querySelector('#qty-sugar')
+    qty_sugar.innerHTML=sugarQuantity
+}
+function totalUpdate(totalQuantity){
+    let qty_total = document.querySelector('#qty-total')
+    qty_total.innerHTML=totalQuantity
+}
+
+// Event listener for clicks on the "+" button for Gingerbread cookies
+gbUp.addEventListener('click', function() {
+    gb++
+    totalCookies++
+    gbUpdate(`${gb}`)
+    totalUpdate(`${totalCookies}`)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+gbDown.addEventListener('click', function() {
+    if(gb>0){
+    gb--
+    totalCookies--
+    }
+    gbUpdate(`${gb}`)
+    totalUpdate(`${totalCookies}`)
+    
+})
+
+ccUp.addEventListener('click', function(e) {
+    cc++
+    totalCookies++
+    ccUpdate(`${cc}`)
+    totalUpdate(`${totalCookies}`)
+})
+
+ccDown.addEventListener('click', function(e) {
+    if(cc>0){
+    cc--
+    totalCookies--
+    }
+    ccUpdate(`${cc}`)
+    totalUpdate(`${totalCookies}`)
+})
+
+sugarUp.addEventListener('click', function(e) {
+    sugar++
+    totalCookies++
+    sugarUpdate(`${sugar}`)
+    totalUpdate(`${totalCookies}`)
+})
+
+sugarDown.addEventListener('click', function(e) {
+    if(sugar>0){
+    sugar--
+    totalCookies--
+    }
+    sugarUpdate(`${sugar}`)
+    totalUpdate(`${totalCookies}`)
+})
